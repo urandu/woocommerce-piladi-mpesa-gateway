17a8226127dd0447aef42c149b3ef19414ed6f4f
urandu <bildad.urandu@jumo.world>
2018-03-25 01:26:36 +0300

    adding custom fields for checkout

--- woocommerce-piladi-mpesa-gateway.php
+++ woocommerce-piladi-mpesa-gateway.php
@@ -3,7 +3,7 @@
 Plugin Name: Piladi - M-pesa WooCommerce Payment Gateway
 Plugin URI: http://woocommerce-piladi-mpesa.piladi.com/
 Description: WooCommerce m-pesa payment gateway integration.
-Version: 1.0
+Version: 1.0.0.1
 */
 
 add_action( 'plugins_loaded', 'cwoa_piladi_mpesa_gateway_init', 0 );
@@ -18,6 +18,46 @@ function cwoa_piladi_mpesa_gateway_init() {
         return $methods;
     }
 }
+
+
+
+add_action('woocommerce_checkout_process', 'process_custom_payment');
+function process_custom_payment(){
+
+    if($_POST['payment_method'] != 'cwoa_add_piladi_mpesa_gateway')
+        return;
+
+    if( !isset($_POST['mobile']) || empty($_POST['mobile']) )
+        wc_add_notice( __( 'Please add your mobile number', $this->domain ), 'error' );
+
+
+    if( !isset($_POST['transaction']) || empty($_POST['transaction']) )
+        wc_add_notice( __( 'Please add your transaction ID', $this->domain ), 'error' );
+
+}
+
+/**
+ * Update the order meta with field value
+ */
+add_action( 'woocommerce_checkout_update_order_meta', 'custom_payment_update_order_meta' );
+function custom_payment_update_order_meta( $order_id ) {
+
+    if($_POST['payment_method'] != 'custom')
+        return;
+
+    // echo "<pre>";
+    // print_r($_POST);
+    // echo "</pre>";
+    // exit();
+
+    update_post_meta( $order_id, 'mobile', $_POST['mobile'] );
+    update_post_meta( $order_id, 'transaction', $_POST['transaction'] );
+}
+
+
+
+
+
 // Add custom action links
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'cwoa_piladi_mpesa_action_links' );
 function cwoa_piladi_mpesa_action_links( $links ) {
--- woocommerce-piladi-mpesa.php
+++ woocommerce-piladi-mpesa.php
@@ -1,27 +1,28 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: urandu
  * Date: 3/24/18
  * Time: 12:26 AM
  */
+class cwoa_WCPiladiMpesa extends WC_Payment_Gateway
+{
 
-
-class cwoa_WCPiladiMpesa extends WC_Payment_Gateway {
-
-    function __construct() {
+    function __construct()
+    {
 
         // global ID
         $this->id = "cwoa_wc_piladi_mpesa_gateway";
 
         // Show Title
-        $this->method_title = __( "Piladi M-pesa", 'wc_piladi_mpesa_gateway' );
+        $this->method_title = __("Piladi M-pesa", 'wc_piladi_mpesa_gateway');
 
         // Show Description
-        $this->method_description = __( "M-pesa Payment Gateway Plug-in for WooCommerce", 'wc_piladi_mpesa_gateway' );
+        $this->method_description = __("M-pesa Payment Gateway Plug-in for WooCommerce", 'wc_piladi_mpesa_gateway');
 
         // vertical tab title
-        $this->title = __( "Piladi M-pesa", 'wc_piladi_mpesa_gateway' );
+        $this->title = __("Piladi M-pesa", 'wc_piladi_mpesa_gateway');
 
 
         $this->icon = null;
@@ -31,6 +32,26 @@ class cwoa_WCPiladiMpesa extends WC_Payment_Gateway {
         // support default form with credit card
         //$this->supports = array( 'default_credit_card_form' );
 
+        /**
+         * Add the field to the checkout page
+         */
+        add_action('woocommerce_after_order_notes', 'customise_checkout_field');
+
+        function customise_checkout_field($checkout)
+        {
+            echo '<div id="customise_checkout_field"><h2>' . __('Heading') . '</h2>';
+            woocommerce_form_field('customised_field_name', array(
+                'type' => 'text',
+                'class' => array(
+                    'my-field-class form-row-wide'
+                ),
+                'label' => __('Customise Additional Field'),
+                'placeholder' => __('Guidence'),
+                'required' => true,
+            ), $checkout->get_value('customised_field_name'));
+            echo '</div>';
+        }
+
         // setting defines
         $this->init_form_fields();
 
@@ -38,7 +59,7 @@ class cwoa_WCPiladiMpesa extends WC_Payment_Gateway {
         $this->init_settings();
 
         // Turn these settings into variables we can use
-        foreach ( $this->settings as $setting_key => $value ) {
+        foreach ($this->settings as $setting_key => $value) {
             $this->$setting_key = $value;
         }
 
@@ -46,142 +67,144 @@ class cwoa_WCPiladiMpesa extends WC_Payment_Gateway {
         ///add_action( 'admin_notices', array( $this,	'do_ssl_check' ) );
 
         // Save settings
-        if ( is_admin() ) {
-            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+        if (is_admin()) {
+            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
         }
     } // Here is the  End __construct()
 
     // administration fields for specific Gateway
-    public function init_form_fields() {
+    public function init_form_fields()
+    {
         $this->form_fields = array(
             'enabled' => array(
-                'title'		=> __( 'Enable / Disable', 'wc_piladi_mpesa_gateway' ),
-                'label'		=> __( 'Enable this payment gateway', 'wc_piladi_mpesa_gateway' ),
-                'type'		=> 'checkbox',
-                'default'	=> 'no',
+                'title' => __('Enable / Disable', 'wc_piladi_mpesa_gateway'),
+                'label' => __('Enable this payment gateway', 'wc_piladi_mpesa_gateway'),
+                'type' => 'checkbox',
+                'default' => 'no',
             ),
             'title' => array(
-                'title'		=> __( 'Title', 'wc_piladi_mpesa_gateway' ),
-                'type'		=> 'text',
-                'desc_tip'	=> __( 'Payment title of checkout process.', 'wc_piladi_mpesa_gateway' ),
-                'default'	=> __( 'M-pesa', 'wc_piladi_mpesa_gateway' ),
+                'title' => __('Title', 'wc_piladi_mpesa_gateway'),
+                'type' => 'text',
+                'desc_tip' => __('Payment title of checkout process.', 'wc_piladi_mpesa_gateway'),
+                'default' => __('M-pesa', 'wc_piladi_mpesa_gateway'),
             ),
             'description' => array(
-                'title'		=> __( 'Description', 'wc_piladi_mpesa_gateway' ),
-                'type'		=> 'textarea',
-                'desc_tip'	=> __( 'Payment title of checkout process.', 'wc_piladi_mpesa_gateway' ),
-                'default'	=> __( 'Pay via mpesa', 'wc_piladi_mpesa_gateway' ),
-                'css'		=> 'max-width:450px;'
+                'title' => __('Description', 'wc_piladi_mpesa_gateway'),
+                'type' => 'textarea',
+                'desc_tip' => __('Payment title of checkout process.', 'wc_piladi_mpesa_gateway'),
+                'default' => __('Pay via mpesa', 'wc_piladi_mpesa_gateway'),
+                'css' => 'max-width:450px;'
             ),
             'consumer_key' => array(
-                'title'		=> __( 'Consumer key', 'wc_piladi_mpesa_gateway' ),
-                'type'		=> 'text',
-                'desc_tip'	=> __( 'This is the consumer key of the c2b app from daraja dev portal.', 'wc_piladi_mpesa_gateway' ),
+                'title' => __('Consumer key', 'wc_piladi_mpesa_gateway'),
+                'type' => 'text',
+                'desc_tip' => __('This is the consumer key of the c2b app from daraja dev portal.', 'wc_piladi_mpesa_gateway'),
             ),
             'consumer_secret' => array(
-                'title'		=> __( 'Consumer secret', 'wc_piladi_mpesa_gateway' ),
-                'type'		=> 'password',
-                'desc_tip'	=> __( 'This is the consumer secret of the c2b app from daraja dev portal.', 'wc_piladi_mpesa_gateway' ),
+                'title' => __('Consumer secret', 'wc_piladi_mpesa_gateway'),
+                'type' => 'password',
+                'desc_tip' => __('This is the consumer secret of the c2b app from daraja dev portal.', 'wc_piladi_mpesa_gateway'),
             ),
             'environment' => array(
-                'title'		=> __( 'Sandbox', 'wc_piladi_mpesa_gateway' ),
-                'label'		=> __( 'Enable Test Mode', 'wc_piladi_mpesa_gateway' ),
-                'type'		=> 'checkbox',
-                'description' => __( 'This is the test mode of gateway.', 'wc_piladi_mpesa_gateway' ),
-                'default'	=> 'no',
+                'title' => __('Sandbox', 'wc_piladi_mpesa_gateway'),
+                'label' => __('Enable Test Mode', 'wc_piladi_mpesa_gateway'),
+                'type' => 'checkbox',
+                'description' => __('This is the test mode of gateway.', 'wc_piladi_mpesa_gateway'),
+                'default' => 'no',
             )
         );
     }
 
     // Response handled for payment gateway
-    public function process_payment( $order_id ) {
+    public function process_payment($order_id)
+    {
         global $woocommerce;
 
-        $customer_order = new WC_Order( $order_id );
+        $customer_order = new WC_Order($order_id);
 
         // checking for transiction
-        $environment = ( $this->environment == "yes" ) ? 'TRUE' : 'FALSE';
+        $environment = ($this->environment == "yes") ? 'TRUE' : 'FALSE';
 
         // Decide which URL to post to
-        $environment_url = ( "FALSE" == $environment )
+        $environment_url = ("FALSE" == $environment)
             ? 'https://secure.authorize.net/gateway/transact.dll'
             : 'https://test.authorize.net/gateway/transact.dll';
 
         // This is where the fun stuff begins
         $payload = array(
             // Authorize.net Credentials and API Info
-            "x_tran_key"           	=> $this->consumer_key,
-            "x_login"              	=> $this->consumer_secret,
-            "x_version"            	=> "3.1",
+            "x_tran_key" => $this->consumer_key,
+            "x_login" => $this->consumer_secret,
+            "x_version" => "3.1",
 
             // Order total
-            "x_amount"             	=> $customer_order->order_total,
+            "x_amount" => $customer_order->order_total,
 
             // Credit Card Information
-            "x_card_num"           	=> str_replace( array(' ', '-' ), '', $_POST['cwoa_authorizenet_aim-card-number'] ),
-            "x_card_code"          	=> ( isset( $_POST['cwoa_authorizenet_aim-card-cvc'] ) ) ? $_POST['cwoa_authorizenet_aim-card-cvc'] : '',
-            "x_exp_date"           	=> str_replace( array( '/', ' '), '', $_POST['cwoa_authorizenet_aim-card-expiry'] ),
+            "x_card_num" => str_replace(array(' ', '-'), '', $_POST['cwoa_authorizenet_aim-card-number']),
+            "x_card_code" => (isset($_POST['cwoa_authorizenet_aim-card-cvc'])) ? $_POST['cwoa_authorizenet_aim-card-cvc'] : '',
+            "x_exp_date" => str_replace(array('/', ' '), '', $_POST['cwoa_authorizenet_aim-card-expiry']),
 
-            "x_type"               	=> 'AUTH_CAPTURE',
-            "x_invoice_num"        	=> str_replace( "#", "", $customer_order->get_order_number() ),
-            "x_test_request"       	=> $environment,
-            "x_delim_char"         	=> '|',
-            "x_encap_char"         	=> '',
-            "x_delim_data"         	=> "TRUE",
-            "x_relay_response"     	=> "FALSE",
-            "x_method"             	=> "CC",
+            "x_type" => 'AUTH_CAPTURE',
+            "x_invoice_num" => str_replace("#", "", $customer_order->get_order_number()),
+            "x_test_request" => $environment,
+            "x_delim_char" => '|',
+            "x_encap_char" => '',
+            "x_delim_data" => "TRUE",
+            "x_relay_response" => "FALSE",
+            "x_method" => "CC",
 
             // Billing Information
-            "x_first_name"         	=> $customer_order->billing_first_name,
-            "x_last_name"          	=> $customer_order->billing_last_name,
-            "x_address"            	=> $customer_order->billing_address_1,
-            "x_city"              	=> $customer_order->billing_city,
-            "x_state"              	=> $customer_order->billing_state,
-            "x_zip"                	=> $customer_order->billing_postcode,
-            "x_country"            	=> $customer_order->billing_country,
-            "x_phone"              	=> $customer_order->billing_phone,
-            "x_email"              	=> $customer_order->billing_email,
+            "x_first_name" => $customer_order->billing_first_name,
+            "x_last_name" => $customer_order->billing_last_name,
+            "x_address" => $customer_order->billing_address_1,
+            "x_city" => $customer_order->billing_city,
+            "x_state" => $customer_order->billing_state,
+            "x_zip" => $customer_order->billing_postcode,
+            "x_country" => $customer_order->billing_country,
+            "x_phone" => $customer_order->billing_phone,
+            "x_email" => $customer_order->billing_email,
 
             // Shipping Information
-            "x_ship_to_first_name" 	=> $customer_order->shipping_first_name,
-            "x_ship_to_last_name"  	=> $customer_order->shipping_last_name,
-            "x_ship_to_company"    	=> $customer_order->shipping_company,
-            "x_ship_to_address"    	=> $customer_order->shipping_address_1,
-            "x_ship_to_city"       	=> $customer_order->shipping_city,
-            "x_ship_to_country"    	=> $customer_order->shipping_country,
-            "x_ship_to_state"      	=> $customer_order->shipping_state,
-            "x_ship_to_zip"        	=> $customer_order->shipping_postcode,
+            "x_ship_to_first_name" => $customer_order->shipping_first_name,
+            "x_ship_to_last_name" => $customer_order->shipping_last_name,
+            "x_ship_to_company" => $customer_order->shipping_company,
+            "x_ship_to_address" => $customer_order->shipping_address_1,
+            "x_ship_to_city" => $customer_order->shipping_city,
+            "x_ship_to_country" => $customer_order->shipping_country,
+            "x_ship_to_state" => $customer_order->shipping_state,
+            "x_ship_to_zip" => $customer_order->shipping_postcode,
 
             // information customer
-            "x_cust_id"            	=> $customer_order->user_id,
-            "x_customer_ip"        	=> $_SERVER['REMOTE_ADDR'],
+            "x_cust_id" => $customer_order->user_id,
+            "x_customer_ip" => $_SERVER['REMOTE_ADDR'],
 
         );
 
         // Send this payload to Authorize.net for processing
-        $response = wp_remote_post( $environment_url, array(
-            'method'    => 'POST',
-            'body'      => http_build_query( $payload ),
-            'timeout'   => 90,
+        $response = wp_remote_post($environment_url, array(
+            'method' => 'POST',
+            'body' => http_build_query($payload),
+            'timeout' => 90,
             'sslverify' => false,
-        ) );
+        ));
 
-        if ( is_wp_error( $response ) )
-            throw new Exception( __( 'There is a connection issue to the payment gateway. Sorry for the inconvenience.', 'wc_piladi_mpesa_gateway' ) );
+        if (is_wp_error($response))
+            throw new Exception(__('There is a connection issue to the payment gateway. Sorry for the inconvenience.', 'wc_piladi_mpesa_gateway'));
 
-        if ( empty( $response['body'] ) )
-            throw new Exception( __( 'There was no response from mpesa.', 'wc_piladi_mpesa_gateway' ) );
+        if (empty($response['body']))
+            throw new Exception(__('There was no response from mpesa.', 'wc_piladi_mpesa_gateway'));
 
         // get body response while get not error
-        $response_body = wp_remote_retrieve_body( $response );
+        $response_body = wp_remote_retrieve_body($response);
 
-        foreach ( preg_split( "/\r?\n/", $response_body ) as $line ) {
-            $resp = explode( "|", $line );
+        foreach (preg_split("/\r?\n/", $response_body) as $line) {
+            $resp = explode("|", $line);
         }
 
         ///complete payment for now
         // Payment successful
-        $customer_order->add_order_note( __( 'Payment received, please await system confirmation.', 'wc_piladi_mpesa_gateway' ) );
+        $customer_order->add_order_note(__('Payment received, please await system confirmation.', 'wc_piladi_mpesa_gateway'));
 
         // paid order marked
         //$customer_order->payment_complete();
@@ -193,8 +216,8 @@ class cwoa_WCPiladiMpesa extends WC_Payment_Gateway {
 
         // Redirect to thank you page
         return array(
-            'result'   => 'success',
-            'redirect' => $this->get_return_url( $customer_order ),
+            'result' => 'success',
+            'redirect' => $this->get_return_url($customer_order),
         );
 //        // values get
 //        $r['response_code']             = $resp[0];
@@ -227,14 +250,16 @@ class cwoa_WCPiladiMpesa extends WC_Payment_Gateway {
     }
 
     // Validate fields
-    public function validate_fields() {
+    public function validate_fields()
+    {
         return true;
     }
 
-    public function do_ssl_check() {
-        if( $this->enabled == "yes" ) {
-            if( get_option( 'woocommerce_force_ssl_checkout' ) == "no" ) {
-                echo "<div class=\"error\"><p>". sprintf( __( "<strong>%s</strong> is enabled and WooCommerce is not forcing the SSL certificate on your checkout page. Please ensure that you have a valid SSL certificate and that you are <a href=\"%s\">forcing the checkout pages to be secured.</a>" ), $this->method_title, admin_url( 'admin.php?page=wc-settings&tab=checkout' ) ) ."</p></div>";
+    public function do_ssl_check()
+    {
+        if ($this->enabled == "yes") {
+            if (get_option('woocommerce_force_ssl_checkout') == "no") {
+                echo "<div class=\"error\"><p>" . sprintf(__("<strong>%s</strong> is enabled and WooCommerce is not forcing the SSL certificate on your checkout page. Please ensure that you have a valid SSL certificate and that you are <a href=\"%s\">forcing the checkout pages to be secured.</a>"), $this->method_title, admin_url('admin.php?page=wc-settings&tab=checkout')) . "</p></div>";
             }
         }
     }
